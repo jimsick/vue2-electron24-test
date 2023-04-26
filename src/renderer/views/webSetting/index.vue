59991d5fe976236945924825da0cd53468c34a53
zhouyanbing <877415861@qq.com>
2023-04-26 16:22:45 +0800

    优化设置页面

--- src/renderer/views/webSetting/index.vue
+++ src/renderer/views/webSetting/index.vue
@@ -3,40 +3,45 @@
     <el-tabs v-model="activeName" class="tabs" type="card" @tab-click="getWebConfig">
       <el-tab-pane label="网站管理" name="first">
         <div class="web-manage">
-          <div class="add-web web-block" @click="doWeb('新增')">
-            <i class="icon iconfont icon-plus-circle add-web-icon"></i>
+          <div class="header-func">
+            <el-input v-model="keyword" placeholder="请输入内容"></el-input>
+            <div class="add-web web-block" @click="doWeb('新增')">
+              <i class="icon iconfont icon-plus-circle add-web-icon"></i>
+            </div>
           </div>
           <div class="config-table">
-            <el-table :data="webList"  height="100%" style="width: 100%">
-            <el-table-column type="index" label="序号" width="50">
-            </el-table-column>
-            <el-table-column prop="webName" label="名称" width="100">
-            </el-table-column>
-            <el-table-column prop="webIcon" label="图标" width="100">
-              <template slot-scope="scope">
-                <i class="icon iconfont" :class="'icon-' + scope.row.webIcon"></i>
-              </template>
-            </el-table-column>
-            <el-table-column prop="webUrl" label="地址">
-            </el-table-column>
-            <el-table-column label="操作">
-              <template slot-scope="scope">
-                <el-button size="mini" @click="doWeb('编辑', scope.row)">编辑</el-button>
-                <el-button size="mini" type="danger" @click="remove(scope.row._id)">删除</el-button>
-              </template>
-            </el-table-column>
-          </el-table>
+            <el-table :data="webList" height="100%" style="width: 100%;background-color: transparent;color: #fff;">
+              <el-table-column type="index" label="序号" width="50">
+              </el-table-column>
+              <el-table-column prop="webName" label="名称" width="100">
+              </el-table-column>
+              <el-table-column prop="webIcon" label="图标" width="100">
+                <template slot-scope="scope">
+                  <i class="icon iconfont" :class="'icon-' + scope.row.webIcon"></i>
+                </template>
+              </el-table-column>
+              <el-table-column prop="webUrl" label="地址">
+              </el-table-column>
+              <el-table-column label="操作">
+                <template slot-scope="scope">
+                  <el-button size="mini" @click="doWeb('编辑', scope.row)">编辑</el-button>
+                  <el-button size="mini" type="danger" @click="remove(scope.row._id)">删除</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
           </div>
-          
+
         </div>
       </el-tab-pane>
     </el-tabs>
     <el-dialog :title="type + '网站'" :visible.sync="webVisible" width="50%" popper-append-to-body
-      :before-close="handleClose">
-      <div class="previwe">
+      :before-close="handleClose" @close="webVisible = false" class="config-dialog">
+      <div class="preview">
         <div class="title">预览</div>
-        <i class="icon iconfont" :class="'icon-' + webParam.webIcon"></i>
-        <span style="float: right; color: #8492a6; font-size: 13px">{{ webParam.webName }}</span>
+        <div class="preview-show">
+          <i class="icon iconfont preview-web-icon" :class="'icon-' + webParam.webIcon"></i>
+          <span style="float: right; color: #8492a6; font-size: 13px">{{ webParam.webName }}</span>
+        </div>
       </div>
       <el-form ref="webParam" :model="webParam" :rules="rules" label-width="80px">
         <el-form-item label="网站名称" prop="webName">
@@ -71,14 +76,32 @@ export default {
   name: 'web-setting',
   components: {
   },
+  watch: {
+    keyword: {
+      immediate: true,
+      deep: false,
+      handler(val) {
+        if (val) {
+          this.webList = this.webList.filter((item) => {
+            return (item.webName.indexOf(val) != -1)
+          })
+        } else {
+          this.webList = JSON.parse(JSON.stringify(this.allData))
+        }
+      }
+    }
+  },
   data() {
     let checkUrl = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('网址不能为空'));
+      }
       let reg = new RegExp("[a-zA-z]+://[^\s]*", "g");
-      console.log(reg.test(value))
-      if (!reg.test(value)) {
-        callback(new Error('网址不符合规范！'));
-      } else {
+      let regIp = new RegExp("[a-zA-z]+://(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]?\d)", "g")
+      if (reg.test(value) || regIp.test(value)) {
         callback();
+      } else {
+        callback(new Error('网址不符合规范！'));
       }
     };
     return {
@@ -98,14 +121,17 @@ export default {
           { min: 2, message: '长度至少2个字符', trigger: 'blur' }
         ],
         webUrl: [
-          { required: true, message: '请输入网站地址', trigger: 'blur' },
-          // { validator: checkUrl, trigger: 'blur' }
+          // { required: true, message: '请输入网站地址', trigger: 'blur' },
+          { validator: checkUrl, trigger: 'blur' }
         ],
         webIcon: [
           { required: true, message: '请选择标志', trigger: 'change' },
         ],
       },
+      // 查询关键字
+      keyword: "",
       webList: [],
+      allData: [],
     }
   },
   created() {
@@ -119,7 +145,8 @@ export default {
       let params = {
         type: 'web-config',
       }
-      this.$db.sort({timestamp: -1}).find(params).then((res, err) => {
+      this.$db.sort({ timestamp: -1 }).find(params).then((res, err) => {
+        this.allData = res
         this.webList = res
       })
     },
@@ -131,7 +158,7 @@ export default {
       if (type == '新增') {
         this.reset();
       } else {
-        this.webParam = row
+        this.webParam = JSON.parse(JSON.stringify(row))
       }
       this.webVisible = !this.webVisible
     },
@@ -144,12 +171,12 @@ export default {
         webUrl: "",
         webName: "",
         englishName: "",
-        
       }
     },
     submit() {
       let params;
       this.$refs["webParam"].validate((valid) => {
+        console.log("valid:", valid)
         if (valid) {
           let reg = new RegExp("[\\u4E00-\\u9FFF]+", "g");
           if (this.type == "新增") {
@@ -170,13 +197,11 @@ export default {
               ...this.webParam
             }
             this.$db.update({ _id: params._id }, params).then((res) => {
-              console.log(res)
               this.webVisible = false
             })
           }
           this.getWebConfig()
         } else {
-          console.log('error submit!!');
           return false;
         }
       });
@@ -208,6 +233,13 @@ export default {
   padding: 0 10px;
 }
 
+.header-func {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
 .web-block {
   width: 30px;
   height: 30px;
@@ -215,7 +247,7 @@ export default {
   align-items: flex-start;
   justify-content: flex-start;
   flex-direction: column;
-  margin-bottom: 10px;
+  margin-left: 10px;
   cursor: pointer;
 }
 
@@ -254,9 +286,51 @@ export default {
   border: none;
 }
 
+/deep/ .el-input__inner {
+  background-color: transparent;
+}
+/deep/ .el-table tr {
+  background-color: transparent;
+}
+/deep/ .el-table--enable-row-hover .el-table__body tr:hover>td.el-table__cell {
+  background-color: #59adff;
+}
+/deep/ .el-table th.el-table__cell {
+  background-color: transparent;
+  color: #fff;
+} 
 .config-table {
   height: 419px;
   width: 100%;
+}
+
+.preview {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.preview .title {
+  font-weight: bold;
+}
+
+.preview-show {
+  width: 78px;
+  height: 78px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 25px;
+}
+
+.preview-web-icon {
+  font-size: 55px;
+}
+
+.config-dialog {
+  -webkit-app-region: no-drag;
 }
 </style>
     
